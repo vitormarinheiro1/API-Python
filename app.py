5d1a871690c43591562c0af550a2e559f6fbf3ff
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-07-15 15:31:01 -0300

    feat: adicionando banco de dados

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+    data = request.json
+    if 'nome' in data and 'price' in data:    
+        product = Product(nome=data["nome"], price=data["price"], description=data.get("description", ""))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product added successfully"})
+    return jsonify({"message": "Invalid product data"}), 400
 
 @app.route('/')
 def hello_world():
